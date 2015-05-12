0feefd3726b8aef5edf45cb5c2415159d4b88e2a
Bruno Campos <bld_bcampos@uolinc.com>
2015-05-12 14:30:51 -0300

    Refatoracao nos testes dos movimentos

--- src/test/java/br/com/elo7/dominio/MovimentosTest.java
+++ src/test/java/br/com/elo7/dominio/MovimentosTest.java
@@ -1,6 +1,6 @@
 package br.com.elo7.dominio;
 
-import junit.framework.Assert;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -30,57 +30,57 @@ public class MovimentosTest {
 		direita.incluiProximaRegra(new MovimentaEsquerda());
 		direita.incluiProximaRegra(new MovimentaFrente());
 		Posicao posicao = direita.movimenta(Comando.ANDAR, Posicao.criar(2, 3, Direcao.NORTE));
-		Assert.assertEquals(Posicao.criar(2, 4, Direcao.NORTE), posicao);
+		assertEquals(Posicao.criar(2, 4, Direcao.NORTE), posicao);
 	}
 	
 	@Test
 	public void deveVirarParaDireitaDoLesteEIrParaSul() {
 		Posicao posicao = direita.movimenta(Comando.DIREITA, Posicao.criar(0, 0, Direcao.LESTE));
-		Assert.assertEquals(Direcao.SUL, posicao.getDirecao());
+		assertEquals(Direcao.SUL, posicao.getDirecao());
 	}
 	
 	@Test
 	public void deveVirarParaDireitaDoNorteEIrParaLeste() {
 		Posicao posicao = direita.movimenta(Comando.DIREITA, Posicao.criar(0, 0, Direcao.NORTE));
-		Assert.assertEquals(Direcao.LESTE, posicao.getDirecao());
+		assertEquals(Direcao.LESTE, posicao.getDirecao());
 	}
 	
 	@Test
 	public void deveVirarParaEsquerdaDoOesteEIrParaSul() {
 		Posicao posicao = esquerda.movimenta(Comando.ESQUERDA, Posicao.criar(0, 0, Direcao.OESTE));
-		Assert.assertEquals(Direcao.SUL, posicao.getDirecao());
+		assertEquals(Direcao.SUL, posicao.getDirecao());
 	}
 	
 	@Test
 	public void deveVirarParaEsquerdaDoLesteEIrParaNorte() {
 		Posicao posicao = esquerda.movimenta(Comando.ESQUERDA, Posicao.criar(0, 0, Direcao.LESTE));
-		Assert.assertEquals(Direcao.NORTE, posicao.getDirecao());
+		assertEquals(Direcao.NORTE, posicao.getDirecao());
 	}
 	
 	@Test
 	public void deveMoverParaFrenteAoLeste() {
 		Posicao posicao = mover.movimenta(Comando.ANDAR, Posicao.criar(1, 1, Direcao.LESTE));
-		Assert.assertEquals(Posicao.criar(2, 1, Direcao.LESTE), posicao);
+		assertEquals(Posicao.criar(2, 1, Direcao.LESTE), posicao);
 	}
 	
 	@Test
 	public void deveMoverParaFrenteAoOeste() {
 		Posicao posicao = mover.movimenta(Comando.ANDAR, Posicao.criar(3, 3, Direcao.OESTE));
-		Assert.assertEquals(Posicao.criar(2, 3, Direcao.OESTE), posicao);
+		assertEquals(Posicao.criar(2, 3, Direcao.OESTE), posicao);
 	}
 	
 	@Test
 	public void deveMoverParaFrenteAoNorte() {
 		RegraMovimentacao regra = new MovimentaFrente();
 		Posicao posicao = regra.movimenta(Comando.ANDAR, Posicao.criar(4, 2, Direcao.NORTE));
-		Assert.assertEquals(Posicao.criar(4, 3, Direcao.NORTE), posicao);
+		assertEquals(Posicao.criar(4, 3, Direcao.NORTE), posicao);
 	}
 	
 	@Test
 	public void deveMoverParaFrenteAoSul() {
 		RegraMovimentacao regra = new MovimentaFrente();
 		Posicao posicao = regra.movimenta(Comando.ANDAR, Posicao.criar(5, 2, Direcao.SUL));
-		Assert.assertEquals(Posicao.criar(5, 1, Direcao.SUL), posicao);
+		assertEquals(Posicao.criar(5, 1, Direcao.SUL), posicao);
 	}
 
 }
